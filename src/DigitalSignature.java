8e1e8b85f06e5ef9dab862252ef985f0acac3624
Bernard O'Connor <bernard.oconnor47@mail.dcu.ie>
2017-11-19 22:11:11 +0000

    Generate S and write hand in info to file

--- src/DigitalSignature.java
+++ src/DigitalSignature.java
@@ -1,4 +1,6 @@
+import javax.xml.bind.DatatypeConverter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -6,15 +8,14 @@ import java.nio.file.Paths;
 import java.security.*;
 import java.util.Random;
 import java.util.Scanner;
-import javax.crypto.*;
 
 public class DigitalSignature {
-    final static String pHexString = "b59dd795 68817b4b 9f678982 2d22594f 376e6a9a bc024184 6de426e5 dd8f6edd"+
+    private final static String P_HEX_STRING = "b59dd795 68817b4b 9f678982 2d22594f 376e6a9a bc024184 6de426e5 dd8f6edd"+
             "ef00b465 f38f509b 2b183510 64704fe7 5f012fa3 46c5e2c4 42d7c99e ac79b2bc"+
             "8a202c98 327b9681 6cb80426 98ed3734 643c4c05 164e739c b72fba24 f6156b6f"+
             "47a7300e f778c378 ea301e11 41a6b25d 48f19242 68c62ee8 dd313474 5cdf7323";
 
-    final static String gHexString = "44ec9d52 c8f9189e 49cd7c70 253c2eb3 154dd4f0 8467a64a 0267c9de fe4119f2" +
+    private final static String G_HEX_STRING = "44ec9d52 c8f9189e 49cd7c70 253c2eb3 154dd4f0 8467a64a 0267c9de fe4119f2" +
             "e373388c fa350a4e 66e432d6 38ccdc58 eb703e31 d4c84e50 398f9f91 677e8864" +
             "1a2d2f61 57e2f4ec 538088dc f5940b05 3c622e53 bab0b4e8 4b1465f5 738f5496" +
             "64bd7430 961d3e5a 2e7bceb6 2418db74 7386a58f f267a993 9833beef b7a6fd68";
@@ -24,6 +25,10 @@ public class DigitalSignature {
     private static BigInteger hexStringToBigInt(String hexString){
         return new BigInteger(hexString.replaceAll("\\s+",""), 16);
     }
+
+
+    private final static BigInteger PRIME_MODULUS_P = hexStringToBigInt(P_HEX_STRING);
+    private final static BigInteger GENERATOR_G = hexStringToBigInt(G_HEX_STRING);
 
     //Used to calculate secret key x
     //TODO link this? https://stackoverflow.com/questions/3735664/randomizing-a-biginteger
@@ -121,47 +126,80 @@ public class DigitalSignature {
     }
 
 
-    private static byte[] readAndHashFile() throws IOException, NoSuchAlgorithmException{
-        //Read in file
+    private static BigInteger[] generateRAndS(BigInteger secretKey, byte[] hashedMessageBytes){
+        //Generate k
+        BigInteger randomK = generateK(PRIME_MODULUS_P.subtract(BigInteger.ONE));
+
+        //Generate r
+        BigInteger r = modularExponentiation(GENERATOR_G,randomK, PRIME_MODULUS_P);
+
+        //calculate H(m)-xr
+        BigInteger hashedMessageBigInt = new BigInteger(hashedMessageBytes); //convert to bigInt
+        BigInteger hmMinusXR = hashedMessageBigInt.subtract(secretKey.multiply(r));
+
+        //calculate k^-1 mod(p-1)
+        BigInteger kMultiplicativeInverse = multiplicativeInverse(randomK, PRIME_MODULUS_P.subtract(BigInteger.ONE));
+
+        //compute s as H(m)-xr * k^-1 mod(p-1)
+        BigInteger s = hmMinusXR.multiply(kMultiplicativeInverse);
+
+        //if s == 0 start over again
+        if(s.compareTo(BigInteger.ZERO) == 0)
+        {
+            BigInteger[] recursiveCall = generateRAndS(secretKey, hashedMessageBytes);
+            r = recursiveCall[0];
+            s = recursiveCall[1];
+        }
+
+        //return r and s
+        return new BigInteger[] {r, s};
+    }
+
+    private static void writeAssignmentHandInToFile(String outputPath,
+                                                    BigInteger publicKeyY,
+                                                    BigInteger[] digitalSignature,
+                                                    byte[] unhashedFile,
+                                                    byte[] hashedFile) throws IOException {
+        PrintWriter printwriter = new PrintWriter(outputPath, "UTF-8");
+        System.out.println("Writing assignment hand-in details to: " + outputPath);
+        printwriter.println("Public Key Y: " + publicKeyY.toString(16));
+        printwriter.println("\nDigital Signature Value R: " + digitalSignature[0].toString(16));
+        printwriter.println("\nDigital Signature Value S: " +  digitalSignature[1].toString(16));
+
+        //Including the unhashed and hashed files just in case.
+        printwriter.println("\nUnhashed File: " + DatatypeConverter.printHexBinary(unhashedFile));
+        printwriter.println("\nHashed File: " + DatatypeConverter.printHexBinary(hashedFile));
+
+        printwriter.close();
+    }
+
+    public static void main(String [] args){
+        //Calculate secret key x
+        BigInteger secretKeyX = bigIntInRange(BigInteger.ONE, PRIME_MODULUS_P.subtract(BigInteger.ONE));
+
+        //Calculate public key y
+        BigInteger publicKeyY = modularExponentiation(GENERATOR_G, secretKeyX, PRIME_MODULUS_P);
+
+        //Read path to file
         Scanner scan = new Scanner(System.in, "UTF-8");
         System.out.println("Enter the path to the file: ");
         Path filePath = Paths.get(scan.next());
+
         try {
+            //read in file
             byte[] fileData = Files.readAllBytes(filePath);
+
+            //hash the file
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            return digest.digest(fileData); //the hashed file
+            byte[] hashedFile = digest.digest(fileData); //the hashed file
 
-        }
-        catch(IOException |NoSuchAlgorithmException e){
-            e.printStackTrace();
-            throw e;
-        }
-    }
+            //Digitally sign the file
+            BigInteger[] digitalSignature = generateRAndS(secretKeyX, hashedFile);
 
+            //Write the assignment results to a file
+            String outputFilePath = filePath.toString() + "-assignment-output.txt";
+            writeAssignmentHandInToFile(outputFilePath, publicKeyY, digitalSignature, fileData, hashedFile);
 
-
-    public static void main(String [] args){
-        BigInteger primeModulusP = hexStringToBigInt(pHexString);
-        BigInteger generatorG = hexStringToBigInt(gHexString);
-
-        //Calculate secret key x
-        BigInteger secretKeyX = bigIntInRange(BigInteger.ONE, primeModulusP.subtract(BigInteger.ONE));
-
-        //Calculate public key y
-        BigInteger publicKeyY = modularExponentiation(generatorG, secretKeyX,primeModulusP);
-
-
-
-        //Generate k
-        BigInteger randomK = generateK(primeModulusP.subtract(BigInteger.ONE));
-
-        //Generate r
-        BigInteger digitalSigntaureValueR = modularExponentiation(generatorG,randomK,primeModulusP);
-
-        //Read in and hash the file
-        try {
-            byte[] hashedFile = readAndHashFile();
-            
         } catch (IOException | NoSuchAlgorithmException e) {
             e.printStackTrace();
             System.exit(-1);
